2964b2a322afe525cc6134263bb9350b3c0f793f
yashchaurasia1999 <96525142+yashchaurasia1999@users.noreply.github.com>
2022-05-16 14:56:41 +0530

    Add files via upload

---
+++ js/burger_fries.js
@@ -0,0 +1,75 @@
+
+function burgerClick(){
+    let button = document.getElementById('burger_button');
+    let bars = document.getElementsByClassName('hb_bar');
+    for(let i = 0; i < bars.length - 1; i++){
+        bars[i].classList.toggle('fry');
+    }
+    bars[bars.length-1].classList.toggle('fry-container');
+    let tomatoes = document.getElementsByClassName('tomato');
+    for(let i = 0; i < tomatoes.length; i++){
+        tomatoes[i].classList.toggle('close-x');
+    }
+}
+// jquery
+$(document).ready(function() {
+  
+
+	  
+    var numItems = $('li.fancyTab').length;
+		
+	
+			  if (numItems == 12){
+					$("li.fancyTab").width('8.3%');
+				}
+			  if (numItems == 11){
+					$("li.fancyTab").width('9%');
+				}
+			  if (numItems == 10){
+					$("li.fancyTab").width('10%');
+				}
+			  if (numItems == 9){
+					$("li.fancyTab").width('11.1%');
+				}
+			  if (numItems == 8){
+					$("li.fancyTab").width('12.5%');
+				}
+			  if (numItems == 7){
+					$("li.fancyTab").width('14.2%');
+				}
+			  if (numItems == 6){
+					$("li.fancyTab").width('16.666666666666667%');
+				}
+			  if (numItems == 5){
+					$("li.fancyTab").width('20%');
+				}
+			  if (numItems == 4){
+					$("li.fancyTab").width('25%');
+				}
+			  if (numItems == 3){
+					$("li.fancyTab").width('33.3%');
+				}
+			  if (numItems == 2){
+					$("li.fancyTab").width('50%');
+				}
+		  
+	 
+
+	
+		});
+
+$(window).load(function() {
+
+  $('.fancyTabs').each(function() {
+
+    var highestBox = 0;
+    $('.fancyTab a', this).each(function() {
+
+      if ($(this).height() > highestBox)
+        highestBox = $(this).height();
+    });
+
+    $('.fancyTab a', this).height(highestBox);
+
+  });
+});
